12be6f063211181a92b96cd4cd0c2c04441ee68e
Christina Floristean <christina.floristean@gmail.com>
2025-11-17 13:57:19 -0500

    Allow local cutlass clone in addition to pip installation

--- openfold3/hacks.py
+++ openfold3/hacks.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 from pathlib import Path
 
@@ -8,6 +9,21 @@ def prep_deepspeed():
 
 
 def prep_cutlass():
+    cutlass_lib_is_installed = importlib.util.find_spec("cutlass_library") is not None
+    cutlass_path = Path(os.environ.get("CUTLASS_PATH", "placeholder"))
+
+    # TODO: This check is for backward compatibility with the old local cutlass setup.
+    #  Remove this and use pip installation only in the future.
+    if not cutlass_lib_is_installed:
+        if not cutlass_path.exists():
+            raise OSError(
+                "CUTLASS_PATH environment variable is not set to a valid path, "
+                "and cutlass_library is not installed. Please install nvidia-cutlass"
+                "via pip or set CUTLASS_PATH to the root of a local cutlass clone."
+            )
+
+        return
+
     # apparently need to set the headers for cutlass
     import cutlass_library
 
